cc6002f99dacfad94fc102e9562ab1ec7498028a
Pedro-Ol1veira <113263052+Pedro-Ol1veira@users.noreply.github.com>
2024-09-29 12:27:27 -0300

    register

---
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,35 @@
+import "./NavBar.css";
+import { NavLink, Link } from "react-router-dom";
+import {
+  BsSearch,
+  BsHouseDoorFill,
+  BsFillPersonFill,
+  BsFillCameraFill,
+} from "react-icons/bs";
+
+const NavBar = () => {
+  return (
+    <nav id="nav">
+      <Link to="/">ReactGram</Link>
+      <form id="search-form">
+        <BsSearch />
+        <input type="text" placeholder="Pesquisar" />
+      </form>
+      <ul id="navLink">
+        <li>
+          <NavLink to="/">
+            <BsHouseDoorFill />
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/login">Entrar</NavLink>
+        </li>
+        <li>
+          <NavLink to="/register">Cadastrar</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
