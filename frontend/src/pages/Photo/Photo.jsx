8278ef8d851e65b4864415671658f5f94ca40b6b
Pedro-Ol1veira <pedru.oliveira865@gmail.com>
2024-10-19 17:42:56 -0300

    comment photo

--- frontend/src/pages/Photo/Photo.jsx
+++ frontend/src/pages/Photo/Photo.jsx
@@ -8,7 +8,7 @@ import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useResetComponentMessage } from "../../hooks/useResetComponentMessage";
 
-import { getPhoto, like } from "../../slices/photoSlice";
+import { getPhoto, like, comment } from "../../slices/photoSlice";
 import PhotoItem from "../../components/Photo/PhotoItem";
 import LikeContainer from "../../components/LikeContainer/LikeContainer";
 
@@ -21,10 +21,24 @@ const Photo = () => {
   const { photo, loading, error, message } = useSelector(
     (state) => state.photo
   );
+  const [commentText, setCommentText] = useState("");
 
   const handleLike = () => {
     dispatch(like(photo._id));
 
+    resetMessage();
+  };
+
+  const handleComment = (e) => {
+    e.preventDefault();
+
+    const commentData = {
+      comment: commentText,
+      id: photo._id,
+    };
+
+    dispatch(comment(commentData));
+    setCommentText("");
     resetMessage();
   };
 
@@ -43,6 +57,39 @@ const Photo = () => {
       <div className="message-container">
         {error && <Message msg={error} type="error" />}
         {message && <Message msg={message} type="success" />}
+      </div>
+      <div className="comments">
+        {photo.comments && (
+          <>
+            <h3>Comentarios: ({photo.comments.length})</h3>
+            <form onSubmit={handleComment}>
+              <input
+                type="text"
+                placeholder="Insira um comentario..."
+                onChange={(e) => setCommentText(e.target.value)}
+                value={commentText || ""}
+              />
+              <input type="submit" value="Enviar" />
+            </form>
+            {photo.comments.length === 0 && <p>Não há comentarios</p>}
+            {photo.comments.map((comment) => (
+              <div className="comment" key={comment.comment}>
+                <div className="author">
+                  {comment.userImage && (
+                    <img
+                      src={`${upload}/users/${comment.userImage}`}
+                      alt={comment.userName}
+                    />
+                  )}
+                  <Link to={`/users/${comment.userId}`}>
+                    <p>{comment.userName}</p>
+                  </Link>
+                </div>
+                <p>{comment.comment}</p>
+              </div>
+            ))}
+          </>
+        )}
       </div>
     </div>
   );
